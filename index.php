16947bf83c1a3fe98bd664e4f89571e37db04986
JuanLariasMendez <jlariasm@miumg.edu.gt>
2022-08-08 20:44:26 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+
+<body>
+    <div id="Inicio">
+        <nav>
+            <ul>
+                <li class="sel"><a href="#">Home</a></li>
+                <li><a href="Tablas.htm">Tablas</a></li>
+                <li><a href="Formularios.html">Formularios</a></li>
+            </ul>
+
+        </nav>
+    </div>
+
+
+    <div class="dospartes">
+        <table>
+            <tr>
+                <td>
+                    <figure>
+                        <img src="/src/Bienvenido.png" alt="Bienvenido" class="dimensionImg">
+                    </figure>
+                </td>
+
+                <td>
+                    <h1>BIENVENIDO</h1>
+                    <p>Al examen parcial de desarrollo<br>Web, en las paginas que usted verá<br>aquí podrá ver el avance que hemos
+                        <br>tenido en las primeras semanas de<br>clase…</p>
+                </td>
+
+            </tr>
+        </table>
+    </div>
+
+
+    <footer>
+        <p>Todos los derechos reservados a: Juan Amilcar Larias Méndez</p>
+    </footer>
+    <link rel="stylesheet" href="styles.css">
+
+</body>
+</html>
